c3fc48c1048e266d9940b0efed8aeab388e5b1d2
Richard Wallis <rdwallis@gmail.com>
2014-08-03 14:26:16 +0200

    Use JSON for priority

--- src/main/java/com/seanchenxi/gwt/storage/client/PriorityStorage.java
+++ src/main/java/com/seanchenxi/gwt/storage/client/PriorityStorage.java
@@ -2,34 +2,35 @@ package com.seanchenxi.gwt.storage.client;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.logging.Logger;
 
 import com.google.gwt.core.client.JavaScriptException;
+import com.google.gwt.json.client.JSONNumber;
+import com.google.gwt.json.client.JSONObject;
+import com.google.gwt.json.client.JSONParser;
+import com.google.gwt.json.client.JSONString;
 import com.google.gwt.storage.client.Storage;
 import com.wallissoftware.zip.client.Inflate;
 
 public class PriorityStorage {
 
-    private final static String PREFIX = "p";
+    private static final String VALUE_KEY = "v";
+    private static final String PRIORITY_KEY = "p";
 
     private final Storage storage;
+
+    private final static Logger logger = Logger.getLogger(PriorityStorage.class.getName());
 
     PriorityStorage(final Storage storage) {
         this.storage = storage;
     }
 
-    private String addPriority(final String data, final int priority) {
-        if (priority < 0) {
-            return data;
-        } else {
-            return PREFIX + "[" + priority + "]" + data;
-        }
-    }
-
     private boolean cleanByPriority() {
-        int maxPriority = -1;
+        double maxPriority = -1;
         final Set<String> keysToDelete = new HashSet<>();
         for (int i = 0; i < getLength(); i++) {
-            final int priority = getPriority(i);
+
+            final double priority = getJsonItem(key(i)).get(PRIORITY_KEY).isNumber().doubleValue();
             if (priority > -1) {
                 if (priority > maxPriority) {
                     maxPriority = priority;
@@ -56,25 +57,28 @@ public class PriorityStorage {
     }
 
     public String getItem(final String key) {
-        final String storageItem = storage.getItem(key);
-        if (storageItem == null) {
+        final JSONObject jsonItem = getJsonItem(key);
+        if (jsonItem.containsKey(VALUE_KEY) && jsonItem.get(VALUE_KEY).isString() != null) {
+            return jsonItem.get(VALUE_KEY).isString().stringValue();
+        } else {
             return null;
         }
-        return stripPriority(Inflate.inflate(storageItem));
+    }
+
+    private JSONObject getJsonItem(final String key) {
+        final String storageItem = storage.getItem(key);
+        if (storageItem == null) {
+            final JSONObject result = new JSONObject();
+            result.put(PRIORITY_KEY, new JSONNumber(-1));
+            return result;
+        }
+        return JSONParser.parseStrict(Inflate.inflate(storageItem)).isObject();
     }
 
     public int getLength() {
         return storage.getLength();
     }
 
-    private int getPriority(final int index) {
-        final String value = getItem(key(index));
-        if (value == null || !value.startsWith(PREFIX)) {
-            return -1;
-        } else {
-            return Integer.valueOf(value.substring(value.indexOf("[") + 1, value.indexOf("]")));
-        }
-    }
 
     public String key(final int index) {
         return storage.key(index);
@@ -86,7 +90,10 @@ public class PriorityStorage {
 
     public void setItem(final String key, final String data, final int priority) {
         try {
-            storage.setItem(key, Inflate.deflate(addPriority(data, priority)));
+            final JSONObject jsonObject = new JSONObject();
+            jsonObject.put(PRIORITY_KEY, new JSONNumber(priority));
+            jsonObject.put(VALUE_KEY, new JSONString(data));
+            storage.setItem(key, Inflate.deflate(jsonObject.toString()));
         } catch (final JavaScriptException e) {
             final String msg = e.getMessage();
             if (msg != null && msg.contains("QUOTA") && msg.contains("DOM")) {
@@ -99,12 +106,6 @@ public class PriorityStorage {
         }
     }
 
-    private String stripPriority(final String value) {
-        if (value == null || !value.startsWith(PREFIX)) {
-            return value;
-        } else {
-            return value.substring(value.indexOf("]") + 1);
-        }
-    }
+
 
 }
